44252efed5f8ca1af807bbcc14fc57ff3a74a239
safa bouguezzi <62938853+safabouguezzi@users.noreply.github.com>
2022-12-14 12:51:46 +0100

    upd link resume.pdf

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -41,7 +41,7 @@ const Header = () => {
 
 
 
-            <div className="tag-cmp app__flex" id="cv"><a href="https://drive.google.com/file/d/1gm6HATi69CYgRORBEPVxInCcbOAbWLtR/view?usp=sharing" rel="noreferrer" target="_blank" className="p-text bold-text aref"> MY RESUME <svg stroke="currentColor"  strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#000" strokeWidth="2" d="M2.99787498,0.999999992 L17.4999998,0.999999992 L20.9999998,4.50000005 L21,23 L3,23 L2.99787498,0.999999992 Z M16,1 L16,6 L21,6 M12,9 L12,18 M8,15 L12,19 L16,15"></path></svg></a></div>
+            <div className="tag-cmp app__flex" id="cv"><a href="https://drive.google.com/file/d/1ZdH5zJ7ha8_1LmqYtLz7riEZM48KXmhX/view?usp=sharing" rel="noreferrer" target="_blank" className="p-text bold-text aref"> MY RESUME <svg stroke="currentColor"  strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke="#000" strokeWidth="2" d="M2.99787498,0.999999992 L17.4999998,0.999999992 L20.9999998,4.50000005 L21,23 L3,23 L2.99787498,0.999999992 Z M16,1 L16,6 L21,6 M12,9 L12,18 M8,15 L12,19 L16,15"></path></svg></a></div>
 
 
 
